794aee53dd3ef5c70535ebfc8731225c85a93c6e
Christian Marcy <christian.marcy@gmail.com>
2018-12-20 18:08:26 -0500

    Added tab

--- react-home-repair/src/components/SiteOwner/HomePage/siteowner_components/siteowner_pages/EveryTicket.js
+++ react-home-repair/src/components/SiteOwner/HomePage/siteowner_components/siteowner_pages/EveryTicket.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import AllTickets from 'components/Provider/Dashboard/dashboard_components/dashboard_pages/AllTickets.js';
 
 export default class EveryTicket extends Component {
-render() {
-  return (
-    <div>
-    <AllTickets/>
-    </div>
-  );
-}
+
+
+  render() {
+    return (
+      <div>
+      <AllTickets/>
+      </div>
+    );
+  }
 }
